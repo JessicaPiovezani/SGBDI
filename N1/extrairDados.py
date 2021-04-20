d7a82b96331ed760416767ad2dcb613e4aa6e5a5
Jessica Piovezani <jessica.piovezani@hotmail.com>
2021-04-19 23:03:07 -0300

    Ajuste

--- N1/extrairDados.py
+++ N1/extrairDados.py
@@ -1,19 +1,21 @@
 import tabula
 import csv
 import pyodbc
-#from conexaoBD import conexaoSQL
+
+#Conexão com BD
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-BPODJN2\SQLEXPRESS;'
                       'Database=BD_AnaliseVendas;'
                       'Trusted_Connection=yes;')
 cursor = conn.cursor()
+
 #Realiza a leitura de todas as páginas do PDF
 #tabelaProdutos = tabula.read_pdf('produtos.pdf', pages='all');
 
 #converte para CSV
 #tabula.convert_into("produtos.pdf", "produtos.csv", output_format="csv", pages='all')
 
-#abrir arquivos csv e realizar a leitura
+#abrir arquivos csv, realizar a leitura e inserção no banco de dados
 with open('categorias.csv') as categorias:
 
   tabelaCategorias = csv.reader(categorias, delimiter=';')
@@ -23,10 +25,11 @@ with open('categorias.csv') as categorias:
     nomeCategoria = l[1]
 
     cursor.execute("""INSERT INTO [DBO].[categorias] ([IdCategoria],[NomeCategoria])  VALUES  ({0}, {1})""")
-    print(idCategoria, nomeCategoria)
+    print("Categoria ID %i inserida com sucesso!" %idCategoria)
 
 conn.conexao.commit()
-"""with open('produtos.csv') as produtos:
+
+with open('produtos.csv') as produtos:
 
   tabelaProdutos = csv.reader(produtos, delimiter=',')
 
@@ -34,4 +37,9 @@ conn.conexao.commit()
     nomeProduto = l[0]
     idCategoria = l[1]
 
-    print(nomeProduto, idCategoria)"""
+    cursor.execute("""INSERT INTO [DBO].[produtos] ([nomeProduto],[idCategoria])  VALUES  ({0}, {1})""")
+    print("Produto" + nomeProduto + "inserido com sucesso!")
+
+conn.conexao.commit()
+
+conn.close
