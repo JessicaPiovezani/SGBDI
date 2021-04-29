3fe8e863e3c1c2e8126dc67ad47f555c709debcf
Jessica Piovezani <jessica.piovezani@hotmail.com>
2021-04-28 23:54:13 -0300

    inserir cupom fiscal

--- N1/insertCupom.py
+++ N1/insertCupom.py
@@ -5,14 +5,14 @@ import pyodbc
 import time
 import string
 
-tempo_inicial = time.time()
-
-i = 1
 conexao = Conexao().get_connection()
 cursor = conexao.cursor()
-while i <= 100000:
 
+tempo_inicial = time.time()
+
+for i in range(10):
     try:
+
         #Randômico para gerar os códigos do cupom
         tamanho = 12
         valores = string.ascii_letters + string.digits + string.punctuation
@@ -24,29 +24,32 @@ while i <= 100000:
         cursor.execute("INSERT INTO cupomFiscal (codigoCupom) VALUES (?)", codigoCupom)
         
         #Recebe o id do cupom recem inserido
-        id_cupom = cursor.lastrowid
+        idCupom = cursor.execute('SELECT @@IDENTITY AS id;').fetchone()[0]
         
         #Cria uma quantidade randomica de itens para esse cupom
-        total_itens = random.randint(1, 20);
+        quantidadeItens = random.randint(1, 20)
         
         #Inicia o loop para registrar os itens do cupom
-        for x in total_itens:
+        for x in range(quantidadeItens):
             
             #Seleciona um produto de forma aleatoria no banco
-            cursor.execute("SELECT idProduto, valorUnitario FROM produtos order by rand() limit 1")
-            produto = cursor.fetchone()
+            cursor.execute("SELECT TOP(1) idProduto, valorUnitario FROM produtos order by NEWID()")
+            produto = cursor.fetchall()[0]
             
             #Define uma quantidade de produtos para o item do cupom
-            quant_produto = random.randint(1, 3)
+            quantidadeProduto = random.randint(1, 3)
             
             #insere o item do cupom
-            cursor.execute("INSERT INTO itensCupom (idProduto, quantidadeProduto, idCupom, valorProduto) VALUES (?,?,?,?)", produto["idProduto"],quant_produto,id_cupom,produto["valorUnitario"])    
+            cursor.execute("INSERT INTO itensCupom (idProduto, quantidadeProduto, idCupom, valorProduto) VALUES (?,?,?,?)", produto.idProduto, quantidadeProduto, idCupom, produto.valorUnitario)    
 
     except (Exception, pyodbc.Error) as error:
         print("Oppss! Algum erro aconteceu :/", error)
         print("ID do cadastro em que o erro aconteceu:", i)
         break
 
-conexao.commit()
+    finally:
+        conexao.commit()
+        print("Cadastro inserido com sucesso!", i)
+
 tempo_final = time.time()
 print("Tempo total de execução: ", tempo_final - tempo_inicial)
